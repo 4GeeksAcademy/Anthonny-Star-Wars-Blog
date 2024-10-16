76657aa552c0fad575e6e7a8ad31750c78c527e3
Anthonny Jhoseph Miranda Prieto <166648867+PugMau@users.noreply.github.com>
2024-10-16 14:39:16 +0000

    Se termino la vista de home

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import starWarsLogo from '../../img/Star_Wars_Logo.png';
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
-	);
+    return (
+        <nav className="custom-navbar">
+            <Link to="/">
+                <img src={starWarsLogo} alt="Star Wars Logo" className="navbar-logo" />
+            </Link>
+            
+            <div className="dropdown ml-auto">
+                <button className="btn btn-warning dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                    Favoritos <span className="favorite-count">(0)</span>
+                </button>
+                <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton">
+                    <li><span className="dropdown-item">No hay favoritos</span></li>
+                </ul>
+            </div>
+        </nav>
+    );
 };
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,78 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import starWarsLogo from '../../img/Star_Wars_Logo_2.png';
+import characters from '../../img/1.png';
+import planets from '../../img/2.png';
+import vehicles from '../../img/3.png';
 import "../../styles/home.css";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
+    <div className="home-container">
+        <img src={starWarsLogo} alt="Star Wars Logo" className="star-wars-logo" />
+        
+        <div className="sections">
+            <div className="section">
+                <img src={characters} alt="Characters" className="section-title" />
+                <div className="card-container">
+                    <div className="card" style={{ width: '18rem' }}>
+                        <img src="https://via.placeholder.com/400x200" className="card-img-top" alt="Character" />
+                        <div className="card-body">
+                            <h5 className="card-title" style={{ fontWeight: 'bold', textAlign: 'left' }}>Character Name</h5>
+                            <p className="card-text" style={{ textAlign: 'left' }}>Gender: Male</p>
+                            <p className="card-text" style={{ textAlign: 'left' }}>Hair Color: Brown</p>
+                            <p className="card-text" style={{ textAlign: 'left' }}>Eye Color: Blue</p>
+                            <div className="d-flex justify-content-between">
+                                <a href="/character-details" className="btn btn-primary">Learn More</a>
+                                <button className="btn btn-warning">
+                                    <i className="fa fa-heart-o"></i> {/* Corazón vacío */}
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Agrega más cards según sea necesario */}
+                </div>
+            </div>
+
+            <div className="section">
+                <img src={planets} alt="Planets" className="section-title" />
+                <div className="card-container">
+                    <div className="card" style={{ width: '18rem' }}>
+                        <img src="https://via.placeholder.com/400x200" className="card-img-top" alt="Planet" />
+                        <div className="card-body">
+                            <h5 className="card-title" style={{ fontWeight: 'bold', textAlign: 'left' }}>Planet Name</h5>
+                            <p className="card-text" style={{ textAlign: 'left' }}>Climate: Temperate</p>
+                            <p className="card-text" style={{ textAlign: 'left' }}>Terrain: Grasslands</p>
+                            <div className="d-flex justify-content-between">
+                                <a href="/planet-details" className="btn btn-primary">Learn More</a>
+                                <button className="btn btn-warning">
+                                    <i className="fa fa-heart-o"></i> {/* Corazón vacío */}
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Agrega más cards según sea necesario */}
+                </div>
+            </div>
+
+            <div className="section">
+                <img src={vehicles} alt="Vehicles" className="section-title" />
+                <div className="card-container">
+                    <div className="card" style={{ width: '18rem' }}>
+                        <img src="https://via.placeholder.com/400x200" className="card-img-top" alt="Vehicle" />
+                        <div className="card-body">
+                            <h5 className="card-title" style={{ fontWeight: 'bold', textAlign: 'left' }}>Vehicle Name</h5>
+                            <p className="card-text" style={{ textAlign: 'left' }}>Model: X-wing</p>
+                            <p className="card-text" style={{ textAlign: 'left' }}>Manufacturer: Incom</p>
+                            <div className="d-flex justify-content-between">
+                                <a href="/vehicle-details" className="btn btn-primary">Learn More</a>
+                                <button className="btn btn-warning">
+                                    <i className="fa fa-heart-o"></i> {/* Corazón vacío */}
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Agrega más cards según sea necesario */}
+                </div>
+            </div>
+        </div>
+    </div>
 );
